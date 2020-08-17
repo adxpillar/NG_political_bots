27d3aef09071cbe9ed22b1f64523b32070149116
adxpillar <dsa.adeagbo@gmail.com>
2020-08-17 00:29:22 -0400

    create db model

--- twitter_service.py
+++ twitter_service.py
@@ -9,10 +9,16 @@ TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
 TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+def twitter_api_client():
+    """
+    args: none
+    returns: api connection
+    """
 
-api = tweepy.API(auth)
-# Test api connection
-print(dir(api))
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    # Test api connection
+    print(dir(api))
 
+    return api 
